0261c23c0a800e13bfcc7298a36e6d1cd65cbd13
msgwise <mikerobertdavis@gmail.com>
2025-07-12 11:27:26 +1000

    Create Sidebar.tsx

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="w-64 bg-gray-800 p-4 hidden md:block">
+      <h2 className="text-lg font-semibold">Filters</h2>
+      <ul>
+        <li>Stocks</li>
+        <li>Forex</li>
+        <li>Crypto</li>
+      </ul>
+      <h2 className="text-lg font-semibold mt-4">Watchlist</h2>
+      {/* Add tickers here */}
+    </aside>
+  );
+};
+
+export default Sidebar;
