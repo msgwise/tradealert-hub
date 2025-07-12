07cc8fd69091fef0c2422ebf9fca6718c087a9b9
msgwise <mikerobertdavis@gmail.com>
2025-07-12 11:28:06 +1000

    Create AudioAlert.tsx

---
+++ src/components/AudioAlert.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Howler from 'howler';
+
+const AudioAlert: React.FC = () => {
+  // Logic moved to NewsFeed for simplicity; this can be expanded
+  return null;
+};
+
+export default AudioAlert;
